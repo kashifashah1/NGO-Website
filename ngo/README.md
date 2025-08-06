eadee04605645c47c03a51985cdbecd06f2947b4
kashifashah1 <kashifa.shah24@gmail.com>
2025-08-06 18:12:24 +0530

    NGO Project Added

---
+++ ngo/README.md
@@ -0,0 +1,2 @@
+<h1>Real-Estate-Project</h1>
+<img src="image.png" alt="Project-Image"/>
---
+++ ngo/src/assets/assets.js
@@ -0,0 +1,101 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Community Teaching",
+      price: "4 hrs/week",
+      location: "California",
+      image: project_img_1
+    },
+    {
+      title: "Tree Plantation Drive",
+      price: "3 hrs/week",
+      location: "San Francisco",
+      image: project_img_2
+    },
+    {
+      title: "Food Distribution Program",
+      price: "Saturdays, 2 hrs",
+      location: "Chicago",
+      image: project_img_3
+    },
+    {
+      title: "Animal Care & Shelter Support",
+      price: "5 hrs/week",
+      location: "Los Angeles",
+      image: project_img_4
+    },
+    {
+      title: "Beach & Park Clean-Up",
+      price: "Sundays, 3 hrs",
+      location: "San Francisco",
+      image: project_img_5
+    },
+    {
+      title: " Elderly Care & Companionship",
+      price: "2 hrs/week",
+      location: "Chicago",
+      image: project_img_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Donald Jackman",
+        title: "Tree Plantation Volunteer",
+        image: profile_img_1,
+        alt: "Portrait of Donald Jackman",
+        rating: 5,
+        text: "I planted 50 trees in my first month of volunteering. It's amazing to contribute to a greener planet.The best time to plant a tree was 20 years ago. The second best time is now"
+    },
+    {
+        name: "Richard Nelson",
+        title: "Community Teaching Volunteer",
+        image: profile_img_2,
+        alt: "Portrait of Richard Nelson",
+        rating: 4,
+        text: "Volunteering to teach kids has been the most fulfilling experience of my life. Seeing them learn and grow brings me immense joy.Education is the most powerful weapon which you can use to change the world."
+    },
+    {
+        name: "James Washington",
+        title: "Food Distribution Volunteer",
+        image: profile_img_3,
+        alt: "Portrait of James Washington",
+        rating: 5,
+        text: "Helping distribute meals to families in need has changed my perspective on life. Every smile makes it worth itWe make a living by what we get, but we make a life by what we give."
+    }
+];
